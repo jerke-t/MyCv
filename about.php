eebc20187d929a3af988ca0523747cfacc117ec9
Jerke_t <Jerke_t@hotmail.com>
2017-02-18 22:09:53 +0100

    Update right pages

---
+++ about.php
@@ -0,0 +1,88 @@
+    <?php 
+        include("navigation/header.php");
+    ?>
+    <section>
+        <h2>Documentatie</h2>
+        <div>
+            <h3>Kleurenschema</h3>
+            <p>
+                Ik heb voor volgend kleurenschema gekozen:
+            </p>
+        </div>
+        <div class="img">
+            <img src="img/kleurenschema.png" alt="kleurschema" align="middle">
+        </div>
+        <article>
+            <p>
+                Aangezien het een CV is, wou ik het zakelijk laten ogen.<br/>
+                Daarom maak ik gebruik van zachte kleuren (wit en grijstinten).<br>
+                Ik wou ook een zekere rust en vertrouwen weerspiegelen. Daarom gebruik ik de blauwtinten<br/>
+                Over deze blauwtinten heb ik het volgende gevonden op het internet<br/>
+            </p>
+            <p style="font-style: italic">
+                "Blauw is een koele, rustige en neutrale kleur. Deze kleur geniet de nodige autoriteit, voornamelijk in de donkere tinten.<br/>
+                Helder blauw is de meest analytische kleur, die verwijst naar trouw, verantwoordelijkheid en veiligheid. <br/>
+                Het blauw van de zee en de lucht opent onze kijk met een zeer breed vizier op alle mogelijkheden en staat om die reden<br/>
+                voor een universele vrijheid en openheid.<br/>
+                De afstand die deze kleur symboliseert, is wel het meest opvallend weergegeven door de adel, die verheven is boven het gewone volk,<br/>
+                want zij hebben blauw bloed. Associaties met koud, afstandelijk, ijzigheid, eenzaamheid zijn dan ook nooit veraf, <br/>
+                maar ze wijzen op plichtsbewustzijn, wijsheid, waarheid en schoonheid."<br/>
+            </p>
+        </article>
+    </section>
+    <section id="wireframe">
+        <h2>Wireframes</h2>
+        <article id="index">
+            <h3>Index</h3>
+            <p>
+                Hieronder vindt u de wireframe voor de indexpagina.
+            </p>
+            <div class="img">
+                <img src="img/wireframe-index.png" alt="Wire frame van de Index pagina" align="middle" width="80%" border="2px">
+            </div>
+        </article>
+        <article id="cv">
+            <h3>CV</h3>
+            <p>
+                Hieronder vindt u de wireframe voor de CV-pagina.
+            </p>
+            <div class="img">
+                <img src="img/wireframe-cv.png" alt="Wire frame van de Index pagina" align="middle" width="80%" border="2px">
+            </div>
+        </article>
+        <article id="contact">
+            <h3>Contactpagina</h3>
+            <p>
+                Hieronder vindt u de wireframe voor de Contact-pagina.
+            </p>
+            <div class="img">
+                <img src="img/wireframe-contact.png" alt="Wire frame van de Index pagina" align="middle" width="80%" border="2px">
+            </div>
+        </article>
+    </section>
+    <section id="php">
+        <h2>
+            PHP
+        </h2>
+        <p>
+            Een bezoeker krijgt de kans om zijn contactgegevens op te geven en een kort bericht.<br/>
+            Met behulp van PHP lees ik deze gegevens uit en geeft ik ze weer.<br/>
+            Ik zet de achternaam in hoofdletters en het kort bericht verschijnt cursief.
+        </p>
+    </section>
+    <section id="javascript">
+        <h2>
+            JavaScript
+        </h2>
+        <article>
+            Via JavaScript en CSS wou ik de knoppen (links naar andere pagina's) dynamisch laten veranderen.<br/>
+            Ik wou hier via javascript de classe laten veranderen en zo ook de nieuwe style laten oproepen<br/>
+            Maar ik bots op het feit dat de HTML pagina steeds herlaadt, daarom dat de classes steeds terug worden gereseted.<br/>
+            Om het toch een beetje te illustreren heb ik in het script een alert laten afgaan die de pagina <br/>
+            tegenhoudt om naar een andere pagina te gaan.<br/>
+            Daarom heb ik dan ook bij de andere li elementen het onclick() event er uit gehaald. 
+        </article>
+    </section>
+    <?php 
+        include("navigation/footer.php");
+    ?>
